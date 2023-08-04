a508a71a8ecf2a2ab1601d1b12cc0d8a47d02b47
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-04 11:12:33 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,53 +5,53 @@ import { FaLinkedin, FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer-items">
-        <div className="footer-web-logo">
+    <div className="footer-items bg-[#893232] text-slate-100">
+      <footer className="footer p-10">
+        <div>
           <img src={AtlanticLogo} alt="footer web logo" />
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
         </div>
-        <div className="footer-items-detail">
-          <div className="footer-contact">
-            <h3>CONTACT(S)</h3>
-            <ul className="contacts-items">
-              <li>+234 (0) 803 465 7847</li>
-              <li>info@atlanticmarineoil.com</li>
-              <li>Port-Harcourt Tel: +234 (0) 818 886 6115</li>
-              <li>Takoradi Tel: +233 24 221 5798</li>
-            </ul>
-          </div>
-          <div className="footer-lagos-office">
-            <h3>LAGOS OFFICE</h3>
-            <p className="lagos-office-details">
-              YMCA House, 1st Floor, 77, Awolowo Road Ikoyi, Lagos, Nigeria
-            </p>
-          </div>
-          <div className="footer-lagos-office">
-            <h3>LAGOS OFFICE</h3>
-            <p className="lagos-office-details">
-              YMCA House, 1st Floor, 77, Awolowo Road Ikoyi, Lagos, Nigeria
-            </p>
-          </div>
-          <div className="footer-port-office">
-            <h3>PORT HARCOURT OFFICE</h3>
-            <p className="lagos-office-details">
-              Atlantic Marine yard, 312 Port Harcourt/Aba Road.
-            </p>
-          </div>
-          <div className="footer-takoradi-office">
-            <h3>TAKORADI OFFICE</h3>
-            <p className="lagos-office-details">
-              Navy Air-base, SOPC/GNPC Shore Base, Takoradi, Ghana
-            </p>
-          </div>
-          <div className="socials">
-            <h3>SOCIALS</h3>
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title">Social</span>
+          <div className="grid grid-flow-col gap-4">
+            {/* <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> */}
+            {/* <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> */}
             <FaLinkedin />
             <FaFacebookF />
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+
+
+
+
+
+    </div>
+
   );
 };
 
