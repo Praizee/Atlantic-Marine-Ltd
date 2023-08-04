e939d1c97ef9be08d2ad185300ff83cbebb37762
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-04 15:34:59 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -18,39 +18,39 @@ const Footer = () => {
           <img src={AtlanticLogo} alt="footer web logo" className="mb-10" />
 
           <span className="footer-title">Contact(s)</span>
-          <a className="link link-hover">+234 (0) 803 465 7847</a>
-          <a className="link link-hover">info@atlanticmarineoil.com</a>
-          <a className="link link-hover">Port-Harcourt Tel: +234 (0) 818 886 6115</a>
-          <a className="link link-hover">Takoradi Tel: +233 24 221 5798</a>
+          <a className="">+234 (0) 803 465 7847</a>
+          <a className="link link-hover" href="mailto:example@mail.com">info@atlanticmarineoil.com</a>
+          <a className="">Port-Harcourt Tel: +234 (0) 818 886 6115</a>
+          <a className="">Takoradi Tel: +233 24 221 5798</a>
         </div>
 
         <div className="pt-28">
           <span className="footer-title">Lagos office</span>
-          <a className="link link-hover">YMCA House,</a>
-          <a className="link link-hover">1st Floor, 77, Awolowo Road</a>
-          <a className="link link-hover">Ikoyi, Lagos, Nigeria</a>
+          <a className="">YMCA House,</a>
+          <a className="">1st Floor, 77, Awolowo Road</a>
+          <a className="">Ikoyi, Lagos, Nigeria</a>
         </div>
 
         <div className="pt-28">
           <span className="footer-title">port harcourt office</span>
-          <a className="link link-hover">Atlantic</a>
-          <a className="link link-hover">Marine yard, 312 Port</a>
-          <a className="link link-hover">Harcourt/Aba</a>
-          <a className="link link-hover">Road.</a>
+          <a className="">Atlantic</a>
+          <a className="">Marine yard, 312 Port</a>
+          <a className="">Harcourt/Aba</a>
+          <a className="">Road.</a>
         </div>
 
         <div className="pt-28">
           <span className="footer-title">takoradi office</span>
-          <a className="link link-hover">Navy Air-base, SOPC/</a>
-          <a className="link link-hover">GNPC Shore Base,</a>
-          <a className="link link-hover">Takoradi, Ghana</a>
+          <a className="">Navy Air-base, SOPC/</a>
+          <a className="">GNPC Shore Base,</a>
+          <a className="">Takoradi, Ghana</a>
         </div>
 
         <div className="pt-28">
           <span className="footer-title">Social</span>
           <div className="grid grid-flow-col gap-4">
-            <LinkedInIcon className="social-icon border bg-[#eae7e741]" />
-            <FacebookIcon className="social-icon border bg-[#eae7e741]" />
+            <LinkedInIcon sx={{ fontSize: "2rem" }} className="social-icon border-none rounded-md p-1 bg-[#eae7e741]" />
+            <FacebookIcon sx={{ fontSize: "2rem" }} className="social-icon border-none rounded-md p-1 bg-[#eae7e741]" />
           </div>
         </div>
       </footer>
