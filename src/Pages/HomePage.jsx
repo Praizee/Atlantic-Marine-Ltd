4178c015839755e5e530f38cbd4d555908f54c8d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-04 11:31:30 +0100

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Home from "../Components/Home/Home"
-import { HeroSection } from '../Components/HeroSection'
 
 const HomePage = () => {
   return (
     <div>
       <Home />
-      <HeroSection />
     </div>
   )
 }
