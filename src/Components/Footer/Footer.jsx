b3a1841233e647f273f08ee9a355fa80b0d4809f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-04 11:21:35 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ const Footer = () => {
       <footer className="footer p-10">
         <div>
           <img src={AtlanticLogo} alt="footer web logo" />
+
           <span className="footer-title">Services</span>
           <a className="link link-hover">Branding</a>
           <a className="link link-hover">Design</a>
