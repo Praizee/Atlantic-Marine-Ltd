fc7c71795ca7f8e46e5090c1739562020c743feb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-04 11:55:59 +0100

    Update DropDown.jsx

--- src/Components/DropDown.jsx
+++ src/Components/DropDown.jsx
@@ -6,7 +6,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function DropDown() {
     return (
         <Menu as="div" className="relative inline-block text-left">
             <div>
