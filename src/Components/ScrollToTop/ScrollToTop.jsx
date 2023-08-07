8f951e8a831fd4fd290dc5c4727c8491e1db116e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-07 17:38:03 +0100

    Update ScrollToTop.jsx

--- src/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Components/ScrollToTop/ScrollToTop.jsx
@@ -8,7 +8,7 @@ const ScrollButton = () => {
 
     const toggleVisible = () => {
         const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300) {
+        if (scrolled > 350) {
             setVisible(true)
         }
         else if (scrolled <= 300) {
