18c5379f6056cbac9b1cc81bb5a7db2a7f68553f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-07 17:37:55 +0100

    Update HeroSection.jsx

--- src/Components/Hero/HeroSection.jsx
+++ src/Components/Hero/HeroSection.jsx
@@ -5,7 +5,7 @@ import HeroImage from "../../assets/hero-image.jpg";
 export const HeroSection = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen bg-base-200 mt-[5rem]">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={HeroImage} className="max-w-sm rounded-sm shadow-2xl" />
                     <div>
@@ -22,3 +22,14 @@ export const HeroSection = () => {
         </div>
     )
 }
+
+{/* <h1 className="text-5xl font-bold">ROV & Robotics</h1>
+<p className="py-6">
+    Diver observation, diverless UWILD, and general inspection <br />
+    of offshore platforms, pipelines, and FPSOs are all major <br />
+    uses for our ROVs.
+</p>
+
+<img src={HeroImage} className="max-w-sm rounded-sm shadow-2xl" />
+
+<button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">Get Started</button> */}
