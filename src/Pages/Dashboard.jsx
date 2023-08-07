fc40b03cea0fb9047a2e2e6d53c5a4ae907c6c3e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-07 15:09:39 +0100

    added new navbar for smaller screens

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import Navbar from "../Components/Navbars/Navbar"
 import Footer from "../Components/Footer/Footer"
 import { HeroSection } from "../Components/Hero/HeroSection";
 import ScrollButton from "../Components/ScrollToTop/ScrollToTop";
+import NewNavBar from "../Components/Navbars/NewNavBar";
 
 
 const Dashboard = () => {
@@ -11,7 +12,7 @@ const Dashboard = () => {
     <section>
       <div>
         <Navbar />
-        < />
+        <NewNavBar />
       </div>
 
       <div className="">
