37ddf0c5f0648bf8f3aefa9b6905a8a8b17dff3b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-08 14:49:32 +0100

    Update Clients.jsx

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from "../../assets/clients-header.png"
+import Logos from "../../assets/logos.png"
+import "./Clients.css";
 
 
 const Clients = () => {
   return (
     <section>
       <div className="mt-[8rem] mb-10">
-        <img src={Header} />
+        <img src={Header} className="w-full" />
+
+        <img src={Logos} className="w-3/4 logos mx-auto pt-[2rem] pb-[.5rem] laptop:pb-[4rem]" />
       </div>
 
     </section>
