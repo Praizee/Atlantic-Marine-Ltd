c802ed9721c10d2ca8486f72475688f36bcc24c6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-09 11:48:19 +0100

    Update QualityPolicy.jsx

--- src/Components/Quality Policy/QualityPolicy.jsx
+++ src/Components/Quality Policy/QualityPolicy.jsx
@@ -5,16 +5,22 @@ const QualityPolicy = () => {
     <section>
 
       <div className="">
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
-              <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <button className="btn btn-primary">Get Started</button>
+
+        <div className="">
+          <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+              <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+              <div>
+                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <p className="py-6">
+                  Atlantic Marine & Oilfield Services Limited is a provider of Subsea Engineering, Diver Intervention, Subsea Construction, Maintenance and Repair services. She is committed to providing quality services that meet the requirements and expectations of its clients for customer satisfaction. Atlantic Marine considers outstanding quality performance an integral measure of overall business success. Management of Atlantic Marine & Oilfield Services shall ensure that all operations are carried out in line with the requirements of the Company’s Quality Management System. Management shall provide adequate
+                </p>
+                <button className="btn btn-primary">Get Started</button>
+              </div>
             </div>
           </div>
         </div>
+
       </div>
 
     </section>
