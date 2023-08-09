d2829646fbac0f6695a43c0cb58774bf09e82101
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-09 13:23:46 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,19 @@ export default {
       'desktop': '1280px',
       // => @media (min-width: 1280px) { ... }
     },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      notbold: '650',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+      black: '900',
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
