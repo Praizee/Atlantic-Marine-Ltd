3336c2ead36b91afa686abb5ffc02668ed0e460b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-09 11:51:00 +0100

    Update QualityPolicy.jsx

--- src/Components/Quality Policy/QualityPolicy.jsx
+++ src/Components/Quality Policy/QualityPolicy.jsx
@@ -12,7 +12,10 @@ const QualityPolicy = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
               <img src={Image} className="max-w-sm rounded-lg shadow-2xl" />
               <div>
-                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <span>
+                  <img src={ } alt='red-rectangle' />
+                  <h1 className="text-5xl font-bold">Quality Policy</h1>
+                </span>
                 <p className="py-6">
                   Atlantic Marine & Oilfield Services Limited is a provider of Subsea Engineering, Diver Intervention, Subsea Construction, Maintenance and Repair services. She is committed to providing quality services that meet the requirements and expectations of its clients for customer satisfaction. Atlantic Marine considers outstanding quality performance an integral measure of overall business success. Management of Atlantic Marine & Oilfield Services shall ensure that all operations are carried out in line with the requirements of the Company’s Quality Management System. Management shall provide adequate
                 </p>
