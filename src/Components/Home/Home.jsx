cf617dbacf6591f0ba0d9d55b1003344638c0a9f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-09 12:16:56 +0100

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroImage from "../../assets/hero-image.jpg"
+import Rectangle from "../../assets/Rectangle 19.png"
 
 const Home = () => {
   return (
@@ -11,13 +12,22 @@ const Home = () => {
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img src={HeroImage} className="max-w-sm rounded-sm shadow-2xl" />
             <div>
-              <h1 className="text-5xl font-bold">ROV & Robotics</h1>
+              <span className='flex'>
+                <img src={Rectangle} alt='red-rectangle' className='mr-2' />
+                <h1 className="text-5xl font-bold">ROV & Robotics</h1>
+              </span>
               <p className="py-6">
                 Diver observation, diverless UWILD, and general inspection <br />
                 of offshore platforms, pipelines, and FPSOs are all major <br />
                 uses for our ROVs.
               </p>
-              <button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">Get Started</button>
+              <button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">
+                View Details
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M10 6L8.59 7.41L13.17 12L8.59 16.59L10 18L16 12L10 6Z" fill="white" />
+                </svg>
+
+              </button>
             </div>
           </div>
         </div>
