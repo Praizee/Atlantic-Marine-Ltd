2ae37d101240dd2d25373d9a0f36bbd858585eeb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-10 10:34:32 +0100

    Update Navbar.jsx

--- src/Components/Navbars/Navbar.jsx
+++ src/Components/Navbars/Navbar.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { NavbarData } from "./NavbarData"
 import AtlanticLogo from "../../assets/AtlanticLogo.png"
-import { BiEnvelope } from "react-icons/bi";
-import "./Navbar.css";
+// import { BiEnvelope } from "react-icons/bi";
+// import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <main id="navbar-components" className="hidden laptop:block tablet:block lg:block bg-base-100 fixed z-[1] top-0 w-full">
-      <div className="hidden laptop:flex smaller:hidden tablet:flex">
+    <main id="navbar-components" className="hidden laptop:px-[12rem] tablet:flex laptop:flex bg-base-100 fixed z-[1] top-0 tablet:w-full laptop:w-full">
+      <div className="hidden laptop:flex smaller:hidden tablet:flex  mt-2">
 
 
         <div className="web-logo">
           <a>
-            <img className="logo" src={AtlanticLogo} alt="web Logo" />
+            <img className="logo laptop:w-[70%] ml-[3rem] mr-0" src={AtlanticLogo} alt="web Logo" />
           </a>
         </div>
 
         <nav tabIndex={0} className="nav-menu">
-          <ul className="nav-menu-items">
+          <ul className="nav-menu-items flex">
             {NavbarData.map((item, index) => {
               return (
-                <li key={index} className={item.className}>
-                  <Link to={item.path} className="list-item">
+                <li key={index} className="">
+                  {/* "flex min-w-max p-4" */}
+                  <NavLink to={item.path} className={({ isActive }) =>
+                    isActive ? 'flex min-w-max p-4 border-b-[3px] border-[#305D96]' : 'flex min-w-max p-4'
+                  }>
                     <span className="item-title">{item.title}</span>
                     <span className="item-icon">{item.icon}</span>
-                  </Link>
+                  </NavLink>
                 </li>
               );
             })}
@@ -35,9 +38,9 @@ const Navbar = () => {
         {/* contact button */}
         <div className="contact-link">
           <Link to="/contact">
-            <button className="btn rounded-md bg-[#305D96] hover:bg-[#204476] text-slate-100">
+            <button className="btn overflow-hidden w-auto flex ml-[3rem] laptop:py-1 laptop:w-auto rounded-md bg-[#305D96] hover:bg-[#204476] text-slate-100">
               Contact
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 block">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
               </svg>
 
@@ -46,7 +49,7 @@ const Navbar = () => {
 
         </div>
       </div>
-    </main>
+    </main >
   );
 };
 
