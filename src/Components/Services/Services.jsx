6a23890c5e6cc81ccdd04c98ca651e2493d6ae9f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-10 10:34:29 +0100

    Update Services.jsx

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -16,20 +16,20 @@ const Services = () => {
               <div className="hero-content flex-col lg:flex-row-reverse">
 
                 <div>
-                  <img src={HeroImage} className="hidden laptop:block max-w-sm mt-8 rounded-lg laptop:w-[50%] w-full mx-auto shadow-2xl laptop:float-right" />
+                  <img src={HeroImage} className="hidden laptop:block max-w-sm  rounded-lg laptop:w-[50%] w-full mx-auto shadow-2xl laptop:float-right" />
 
                   <span className='flex'>
                     <img src={Rectangle} alt='red-rectangle' className='mr-2 float-right' />
                     <h1 className="laptop:text-4xl text-3xl font-bold">ROV & Robotics</h1>
                   </span>
 
-                  <img src={HeroImage} className="laptop:hidden block max-w-sm mt-8 rounded-lg laptop:w-[40%] w-full mx-auto shadow-2xl laptop:float-right" />
+                  <img src={HeroImage} className="laptop:hidden block max-w-sm mt-8 laptop:w-[40%] w-full mx-auto laptop:float-right" />
 
-                  <p className="pb-2 pt-6 mt-4 font-notbold laptop:max-w-[80%] laptop:pr-4 leading-[1.5rem]">
+                  <p className="pb-2 pt-6 mt-4 font-semibold laptop:max-w-[80%] laptop:pr-4 leading-[2rem]">
                     Atlantic Marine can display its complete ownership of a fleet of five (5nos) inspection class ROVs. From Subsea Tech mini Guardian ROV to the Turtuga ROV, SeaEye FALCON with its accompanying Zone 2 rated control van and the Sub Atlantic MOJAVE with a 10ft control van including the Inuktun MaggHD Crawler. All five ROVs can be used for a variety of offshore operations including diver observation, diverless UWILD and general inspection of offshore platforms, pipelines and FPSOs.
                   </p>
 
-                  <p className="pt-2 pb-6 font-notbold laptop:max-w-[87%] leading-[2.5rem]">
+                  <p className="pt-2 pb-6 font-semibold laptop:max-w-[87%] leading-[2.5rem]">
                     <a href='../../assets/Docs/BrochureMiniROVGuardianEN.pdf' target='_blank' className='link hover:no-underline block text-blue-500 laptop:w-[35%] w-[85%]'>GUARDIAN mini ROV Specification</a>
                     <a href='../../assets/Docs/FalconROVSpecs.pdf' target='_blank' className='link hover:no-underline block text-blue-500 laptop:w-[21%] w-[60%]'>SeaEye FALCON ROV</a>
                     <a href='../../assets/Docs/AtlanticMarineSubAtlanticMojave2018.pdf' target='_blank' className='link hover:no-underline block text-blue-500 laptop:w-[27%] w-[63%]'>Sub Atlantic MOJAVE ROV</a>
