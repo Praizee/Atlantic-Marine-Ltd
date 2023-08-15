5fe7418b1862d66782cffb12eadb24f5f32c73ea
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-15 11:38:49 +0100

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -19,13 +19,22 @@ const AboutUs = () => {
               </span>
 
               <p className="py-6 leading-8 w-full">
-                Atlantic Marine & Oilfield Services is a Subsea Engineering - Diving Company since 1999. She is an ISO 9001-2015. Atlantic Marine is also an ABS, Bureau Veritas, and Lloyds Class In–Water Survey Company.
+                {/* <br /> */}
+                <p>
+                  Atlantic Marine & Oilfield Services is a Subsea Engineering - Diving Company since 1999. She is an ISO 9001-2015. Atlantic Marine is also an ABS, Bureau Veritas, and Lloyds Class In–Water Survey Company.
+                </p>
 
-                Atlantic Marine is fully equipped with Classed Air Dive Spread Equipment, Inspection Class ROV’s, Robot-aided Subsea Engineering Tooling and Equipment, Zone 2 Offshore Work Containers, Divers Launch and Recovery Spread and Subsea Inspection, Construction and Welding Equipment.
+                <p>
+                  Atlantic Marine is fully equipped with Classed Air Dive Spread Equipment, Inspection Class ROV’s, Robot-aided Subsea Engineering Tooling and Equipment, Zone 2 Offshore Work Containers, Divers Launch and Recovery Spread and Subsea Inspection, Construction and Welding Equipment.
+                </p>
 
-                And with the recent purpose built LightDiveBoat (LDB) acquisution, MV. Eghenoflorence, Atlanticmarine can mobilise on very short notice for most dive operations.
+                <p>
+                  And with the recent purpose built LightDiveBoat (LDB) acquisution, MV. Eghenoflorence, Atlanticmarine can mobilise on very short notice for most dive operations.
+                </p>
 
-                Atlantic Marine has its head office in Lagos Nigeria, a Yard base in Portharcourt Nigeria and a satellite office in Accra Ghana.
+                <p>
+                  Atlantic Marine has its head office in Lagos Nigeria, a Yard base in Portharcourt Nigeria and a satellite office in Accra Ghana.
+                </p>
 
               </p>
               <button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">
