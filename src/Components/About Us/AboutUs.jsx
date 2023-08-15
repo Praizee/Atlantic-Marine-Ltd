b3d8b7bbe719c861c803d5f250bcc62807c4a583
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-15 13:01:20 +0100

    Update AboutUs.jsx

--- src/Components/About Us/AboutUs.jsx
+++ src/Components/About Us/AboutUs.jsx
@@ -77,6 +77,41 @@ const AboutUs = () => {
           </div>
           {/* END OF MISSION AND VISION */}
 
+          {/* LATEST NEWS */}
+          <div className="laptop:mx-[8.7rem]">
+            <span className="flex mb-6">
+              <img src={Rectangle} alt='red-rectangle' className='mr-2 w-[0.3rem]' />
+              <h1 className="text-3xl font-bold">Latest News</h1>
+            </span>
+
+            <h1 className="text-2xl font-bold">Atlantic Marine won Company of the year</h1>
+            <p className="py-4 leading-2 w-full">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in fusce purus sed. Et tellus, laoreet in lorem ac cras non purus tortor. Placerat sed non a justo, pellentesque ac. Donec quam ac massa, egestas arcu. Sed velit aliquam vel aliquam quis placerat. Vitae congue pellentesque fringilla proin sit sollicitudin ornare nunc. Tincidunt ipsum, pellentesque in eu egestas est.
+            </p>
+
+            <button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">
+              View Details
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 6L8.59 7.41L13.17 12L8.59 16.59L10 18L16 12L10 6Z" fill="white" />
+              </svg>
+            </button>
+
+            {/* NEWS II */}
+            <h1 className="text-2xl font-bold">Atlantic Marine won Company of the year</h1>
+            <p className="py-4 leading-2 w-full">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in fusce purus sed. Et tellus, laoreet in lorem ac cras non purus tortor. Placerat sed non a justo, pellentesque ac. Donec quam ac massa, egestas arcu. Sed velit aliquam vel aliquam quis placerat. Vitae congue pellentesque fringilla proin sit sollicitudin ornare nunc. Tincidunt ipsum, pellentesque in eu egestas est.
+            </p>
+
+            <button className="btn rounded-none hover:bg-[#204476] bg-[#305D96] text-slate-100">
+              View Details
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 6L8.59 7.41L13.17 12L8.59 16.59L10 18L16 12L10 6Z" fill="white" />
+              </svg>
+            </button>
+
+          </div>
+          {/* END OF LATEST NEWS */}
+
         </div>
       </div>
 
