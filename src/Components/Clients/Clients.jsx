fef8e8d98b26e709ca943a738cff85bd0781f343
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-15 13:55:11 +0100

    Update Clients.jsx

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -7,7 +7,7 @@ import "./Clients.css";
 const Clients = () => {
   return (
     <section>
-      <div className="laptop:mt-[7rem] mt-[5rem] mb-8">
+      <div className="laptop:mt-[7rem] mt-[5rem] mb-8 text-[#010037]">
         <img src={Header} className="w-full " />
 
         <img src={Logos} className="w-3/4 logos mx-auto pt-[2rem] pb-[.5rem] laptop:pb-[4rem]" />
