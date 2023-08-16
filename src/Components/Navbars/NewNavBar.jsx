02feafc180b1b9c10628a48c5afd2ec83359dc4e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 16:01:10 +0100

    Update NewNavBar.jsx

--- src/Components/Navbars/NewNavBar.jsx
+++ src/Components/Navbars/NewNavBar.jsx
@@ -1,68 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { NavbarData } from "./NavbarData"
 import AtlanticLogo from "../../assets/AtlanticLogo.png"
-import { Menu, Transition } from '@headlessui/react'
-
 // import { BiEnvelope } from "react-icons/bi";
 // import "./Navbar.css";
 
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
-
-
 const NewNavBar = () => {
-    return (
-        <main id="">
-            <div className="flex laptop:hidden smaller:flex w-full tablet:hidden fixed z-[1] top-0">
-                {/* <a className="btn btn-ghost normal-case text-xl">
-                    <img className="web-logo" src={AtlanticLogo} alt="web Logo" />
-                </a> */}
+    const [showNav, setshowNav] = useState(true)
 
+    // const showNav = () => {
+    //     console.log('okay')
+    //     setShowNav(true)
+    // }
+    return (
+        <main className="">
+            <div className="flex laptop:hidden smaller:flex w-full tablet:hidden fixed z-[10] bg-white">
                 {/* navbar */}
-                <div className="navbar bg-base-100 w-full">
+                <div className="navbar bg-white w-full">
                     <div className="navbar-start">
 
-                        <div className="dropdown">
-                            <Menu as="div" className="relative inline-block text-left">
+                        {/* 'hamburger' icon */}
+                        <label onClick={() => setshowNav(!showNav)}
+                            tabIndex={0} className="btn flex bg-white border-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 -ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        {/* end of icon */}
+                    </div>
 
-                                {/* 'hamburger' icon */}
-                                <Menu.Button tabIndex={0} className="btn btn-ghost lg:hidden flex">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 -ml-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                                </Menu.Button>
-                                {/* end of icon */}
-
-                                <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-
-                                    <Menu.Items tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-black rounded-box w-52">
-                                        {NavbarData.map((item, index) => {
-                                            return (
-                                                <Menu.Item key={index} className={item.className}>
-                                                    <Link to={item.path} className="">
-                                                        <span className="">{item.title}</span>
-                                                        <span className="">{item.icon}</span>
-                                                    </Link>
-                                                </Menu.Item>
-                                            );
-                                        })}
-                                    </Menu.Items>
-                                </Transition>
-                            </Menu>
-                        </div>
-                        <a className="btn btn-ghost normal-case text-xl w-full -mr-5">
-                            <img className="w-full tablet:w-[60%] laptop:w-[20%] smaller:w-[80%]" src={AtlanticLogo} alt="web Logo" />
+                    <div className=" w-[50%]">
+                        <a className="">
+                            <img className="web-logo" src={AtlanticLogo} alt="web Logo" />
                         </a>
                     </div>
 
+                    {/* contact button */}
                     <div className="navbar-end">
                         <Link to="/contact" className="btn rounded-md bg-[#305D96] hover:bg-[#204476] text-slate-100">
                             Contact
@@ -71,12 +42,29 @@ const NewNavBar = () => {
                             </svg>
                         </Link>
                     </div>
-
+                    {/* end of contact button */}
                 </div>
-                {/* end of navbar */}
-
             </div>
-        </main>
+
+            {/* navbar content */}
+            {showNav && <nav tabIndex={0} className="mt-[5rem] p-2 absolute h-full font-medium z-10 bg-[#893232] text-white rounded-box w-full">
+                <ul>
+                    {NavbarData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.className}>
+                                <Link to={item.path} className="">
+                                    <span className="">{item.title}</span>
+                                    <span className="">{item.icon}</span>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>}
+            {/* end of navbar content */}
+            {/* end of navbar */}
+
+        </main >
     );
 };
 
