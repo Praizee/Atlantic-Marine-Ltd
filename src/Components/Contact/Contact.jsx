be6d0c203e823f84fb0911248b1849fc5113c383
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 09:38:05 +0100

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
 
             <h1 className="text-2xl font-bold">Send us a message</h1>
 
-            <div className="bg-[#BDBDBD] p-8 rounded">
+            <div className="bg-[#f3eeee] overflow-hidden p-8 rounded">
               <form onSubmit={handleSubmit} class="w-full max-w-lg">
                 <div class="flex flex-wrap -mx-3 mb-6">
                   <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -53,36 +53,8 @@ const Contact = () => {
                     <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
                   </div>
                 </div>
-                <div class="flex flex-wrap -mx-3 mb-2">
-                  <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                      City
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-                  </div>
-                  <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                      State
-                    </label>
-                    <div class="relative">
-                      <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                        <option>New Mexico</option>
-                        <option>Missouri</option>
-                        <option>Texas</option>
-                      </select>
-                      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                      Zip
-                    </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
-                  </div>
-
-                  <button type='submit' className="btn capitalize font-notbold overflow-y-hidden laptop:py-1 ml-10 rounded-md border-none bg-[#305D96] hover:bg-[#204476] text-slate-100 hover:text-slate-100">
+                <div>
+                  <button type='submit' className="btn px-8 py-1 capitalize font-notbold overflow-y-hidden rounded-md border-none bg-[#305D96] hover:bg-[#204476] text-slate-100 hover:text-slate-100">
                     Send
                   </button>
                 </div>
