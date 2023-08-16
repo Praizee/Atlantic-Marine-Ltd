8825273c119af61703e45870ae29dc6912013740
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-08-16 10:40:55 +0100

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -27,32 +27,35 @@ const Contact = () => {
 
             <h1 className="text-2xl font-bold">Send us a message</h1>
 
-            <div className="bg-[#f3eeee] overflow-hidden p-8 rounded-lg">
+            <div className="bg-[#f3eeee] laptop:w-[90%] overflow-hidden p-8 rounded-lg mt-5 laptop:mb-12">
               <form onSubmit={handleSubmit} class="w-full max-w-lg">
                 <div class="flex flex-wrap -mx-3 mb-6">
-                  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                  <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0 laptop:flex">
+                    {/* <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                       First Name
-                    </label>
-                    <input class="laptop:appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                    <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                    </label> */}
+                    <input class="laptop:appearance-none mb-12 laptop:mb-0 laptop:mr-8 block w-full laptop:w-1/2 bg-white border rounded py-3 px-4 leading-tight focus:outline-none focus:border-[#3686FE80] text-[#010037]"
+                      id="grid-last-name" type="text" placeholder="Name" />
+
+                    <input class="laptop:appearance-none block w-full laptop:w-1/2 bg-white border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:border-[#3686FE80] text-[#010037]"
+                      id="grid-last-name" type="email" placeholder="Email" />
                   </div>
-                  <div class="w-full md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                      Last Name
-                    </label>
-                    <input class="laptop:appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
-                  </div>
+
                 </div>
+                <div class="w-full md:w-1/2 px-3 -mx-3 mb-6 md:mb-0 laptop:flex">
+                  <input class="laptop:appearance-none mb-12 laptop:mb-10 laptop:mr-8 block laptop:w-1/2 bg-white border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#3686FE80] text-[#010037]"
+                    id="grid-last-name" type="text" placeholder="Subject" />
+                </div>
+
                 <div class="flex flex-wrap -mx-3 mb-6">
                   <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                      Password
-                    </label>
-                    <input class="laptop:appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                    <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                    <textarea class="laptop:appearance-none resize-y block w-full bg-white border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-[#3686FE80] text-[#010037]"
+                      id="grid-password" type="text" placeholder="Type your message" />
+                    {/* <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p> */}
                   </div>
                 </div>
+
+                {/* button */}
                 <div>
                   <button type='submit' className="btn px-8 py-1 capitalize font-notbold overflow-y-hidden rounded-md border-none bg-[#305D96] hover:bg-[#204476] text-slate-100 hover:text-slate-100">
                     Send
